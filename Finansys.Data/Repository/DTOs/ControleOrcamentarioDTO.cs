a6c5514c6a647e790acd6fcac7dd5ff2cef50f0b
João Moreira <joaobraghin.moreira@gmail.com>
2021-07-26 21:07:09 -0300

    Commit antes das alterações do controle orçamentario

--- Finansys.Data/Repository/DTOs/ControleOrcamentarioDTO.cs
+++ Finansys.Data/Repository/DTOs/ControleOrcamentarioDTO.cs
@@ -8,5 +8,26 @@ namespace Finansys.Data.Repository.DTOs
 {
     class ControleOrcamentarioDTO
     {
+        [Key]
+        [MaxLength(50)]
+        public string CategoriaId { get; set; }
+
+        [MaxLength(50)]
+        public string UsuarioId { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Descricao { get; set; }
+
+        public CategoriaDTO(string CategoriaId, string usuarioId, string name, string descricao)
+        {
+            this.CategoriaId = CategoriaId;
+            UsuarioId = usuarioId;
+            Name = name;
+            Descricao = descricao;
+        }
     }
+}
 }
